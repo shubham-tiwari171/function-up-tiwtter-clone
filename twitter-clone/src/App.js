a190f37e3992b1c8b36be1eecea61d79456af09a
ShubhamJadhav21 <shubhamjadhavs128@gmail.com>
2023-05-30 14:30:48 +0530

    feature SignUp has uploaded

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUp from './components/organisms/SignUp/SignUp';
+
 function App() {
   return (
+    <div>
+      <SignUp/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div className="App">App</div>}></Route>
+        
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
