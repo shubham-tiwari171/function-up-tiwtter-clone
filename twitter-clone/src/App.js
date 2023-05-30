d75125336b04c274a886b22c7386ff5fa49a66c7
Shubham <tshubhamucem@gmail.com>
2023-05-30 10:13:42 +0530

    sideNav bar

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Sidenavbar from './components/molecules/Sidenavbar/Sidenavbar';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div className="App">App</div>}></Route>
+        <Route path="/" element={<div className="App"><Sidenavbar /></div>}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
