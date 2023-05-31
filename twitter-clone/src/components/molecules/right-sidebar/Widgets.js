8a281d69b74085db264e43f33e79d95d5625e830
Neha <nehatualea@gmail.com>
2023-06-01 01:38:19 +0530

    fix: UI fixed

--- twitter-clone/src/components/molecules/right-sidebar/Widgets.js
+++ twitter-clone/src/components/molecules/right-sidebar/Widgets.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import styles from './widgets.module.css'
-import {CiSearch} from 'react-icons/ci'
-import Trending from './Trending'
+import React from "react";
+import styles from "./widgets.module.css";
+import { CiSearch } from "react-icons/ci";
+import Trending from "./Trending";
 
 const Widgets = () => {
-    
-
   return (
     <div className={styles.widgets}>
       <div className={styles.search}>
-          <input className={styles['search-tweets']} type="text" placeholder='Search Twitter'/>
-          <div className={styles['search-icon']}>
-            <CiSearch/>
-            </div>
+        <input
+          className={styles["search-tweets"]}
+          type="text"
+          placeholder="Search Twitter"
+        />
+        <div className={styles["search-icon"]}>
+          <CiSearch />
         </div>
-        <Trending/>
-           
+      </div>
+      <Trending />
     </div>
-  )
-}
+  );
+};
 
-export default Widgets
+export default Widgets;
