fc2950d84eb03f8e80d19b8facf90b9eb84b4f6e
Neha <nehatualea@gmail.com>
2023-05-31 13:51:10 +0530

    created component

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Widgets from './components/molecules/right-sidebar/Widgets';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div className="App">App</div>}></Route>
+        <Route path="/" element={<Widgets />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ twitter-clone/src/components/molecules/right-sidebar/Trending.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import styles from './trending.module.css'
+
+
+const Trending = () => {
+    
+    const trendingNow=[{
+        id:1,
+        country:"India",
+        whatsTrending:"Scripted",
+        totalTweets:233
+    },
+    {
+        id:2,
+        country:"India",
+        whatsTrending:"IPL",
+        totalTweets:255
+    },
+    {
+        id:3,
+        country:"India",
+        whatsTrending:"Mr India",
+        totalTweets:558
+    },
+    {
+        id:4,
+        country:"India",
+        whatsTrending:"#Parineeti",
+        totalTweets:559
+    }
+]
+  return (
+    <div className={styles.container}>
+        <div className={styles.wrapper}>
+        <div className={styles.heading}>
+            <h4>Whats Happening</h4>
+        </div>
+        {
+            trendingNow.map((item)=>(
+                <div key={item.id} className={styles['trending-now']}>
+                       <div className={styles.country}>
+            Trending in {item.country}
+        </div>
+        <div className={styles.name}>
+            <strong>{item.whatsTrending}</strong>
+
+        </div>
+        <div className={styles.tweets}>
+        {item.totalTweets}k Tweets
+        </div>
+                </div>
+            ))
+        }
+        <div className={styles['show-more']}>
+            <div className={styles['show-button']}>Show more</div>
+        </div>
+        </div>
+
+        
+
+    </div>
+  )
+}
+
+export default Trending
