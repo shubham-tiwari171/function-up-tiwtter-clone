be56330a655ef72f58b2106bc80c6e9f9bed9423
BABLU KUMAR <bablukmr800@gmail.com>
2023-05-31 12:17:58 +0530

    fix: login css

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,18 +1,19 @@
-
-import logo from './logo.svg';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/molecules/login/Login';
 import SignUp from './components/organisms/SignUp/SignUp';
 import Sidenavbar from './components/molecules/Sidenavbar/Sidenavbar';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={< Login />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/home" element={<Sidenavbar />}></Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Sidenavbar />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
+
 export default App;
--- twitter-clone/src/components/molecules/login/Login.js
+++ twitter-clone/src/components/molecules/login/Login.js
@@ -1,72 +1,90 @@
-import React, { useState } from 'react'
-import styles from './Login.module.css'
-import { Link } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-
-
+import React, { useState, useEffect } from 'react';
+import styles from './Login.module.css';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
-
-    const redirectHome = useNavigate()
-    const email = 'bk'
-    const password = 'bk@'
-
-    const [inputValue, setInputValue] = useState({ email: '', password: '' })
+    const redirectHome = useNavigate();
+    const [inputValue, setInputValue] = useState({ name: '', email: '' });
+    
+    useEffect(() => {
+        const getUserData = localStorage.getItem('userData');
+        setInputValue((prevState) => ({
+            ...prevState,
+            ...JSON.parse(getUserData),
+        }));
+    }, []);
 
     function getData(e) {
-        console.log(e.target.value)
-        setInputValue(e.target.value)
+        setInputValue((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
     }
-
+    console.log(inputValue)
     function handleSubmit(e) {
         e.preventDefault();
-
-        const getUserArr = localStorage.getItem("userData")
-        if (getUserArr && getUserArr.length) {
-            const userData = JSON.parse(getUserArr);
-            const userLogin = userData.filter((element, i) => {
-                return element.email === email && element.password === password
-            })
-            if (userLogin.length === 0) {
-                alert('invalid Details')
+        const getUserArr = localStorage.getItem('userData');
+        if (getUserArr) {
+            try {
+                const userData = JSON.parse(getUserArr);
+                const { name, email } = inputValue;
+                const userLogin = userData.filter((element) => {
+                    return element.name === name && element.email === email;
+                });
+                if (userLogin.length === 0) {
+                    alert('Invalid details');
+                } else {
+                    alert('Conform to go on Home');
+                    redirectHome('/home');
+                }
+            } catch (error) {
+                console.error('Error parsing userData:', error);
+                alert('User data is invalid');
             }
-            else {
-                redirectHome('/Home')
-            }
-
+        } else {
+            alert('User data not found');
         }
-
     }
 
     return (
+        
         <div className={styles.Login}>
             <div className={styles.Logo}>
-                <img src='./twitter.png' />
+                <img alt='twitter' src='./twitter.png' />
                 <h2>Sign in to Twitter</h2>
                 <button>
-                    <img src='./google.png' />
+                    <img alt='google' src='./google.png' />
                     Sign in with Google
                 </button>
                 <button>
-                    <img src='./apple.png' />
+                    <img alt='apple' src='./apple.png' />
                     Sign in with Apple
                 </button>
-                <hr></hr>
+                <hr />
                 <span>or</span>
                 <form onSubmit={handleSubmit}>
-                    <input type='email' placeholder='user email'
-                        name='email' onChange={getData}
+                    <input
+                        type='text'
+                        name='name'
+                        placeholder='User Name'
+                        value={inputValue.name}
+                        onChange={getData}
                     />
-                    <input type='password' placeholder='password'
-                        name='password' onChange={getData}
-                    /><br />
-                    <Link to='/password_reset'>Forgot password?	</Link>
+                    <input
+                        type='email'
+                        name='email'
+                        placeholder='Email'
+                        value={inputValue.email}
+                        onChange={getData}
+                    />
+                    <br /><br />
+                    <Link to='/password_reset'>Forgot password? </Link>
                     <div className={styles.Buttons}>
-                        <button> Log in</button>
+                        <button>Log in</button>
                     </div>
                 </form>
-                Don't have an account? <Link to='/signup'> Sign Up</Link>
+                Don't have an account? <Link to='/signup'>Sign Up</Link>
             </div>
         </div>
-    )
+    );
 }
