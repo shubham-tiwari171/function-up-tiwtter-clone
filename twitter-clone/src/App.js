200bbbb1d55ef56b8d79f2ce8b0570d5ca72e0bb
pragati-sriv <ps028ideal@gmail.com>
2023-06-01 10:02:55 +0530

    feature post_section

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import PostSection from "./components/molecules/Post_Section"
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
+    <div>
+    <PostSection/>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div className="App">App</div>}></Route>
+        <Route path="/" element={<div className="App"></div>}></Route>
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
